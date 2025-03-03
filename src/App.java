7e6d3b433b5e9bb2e28e85da6f8bf5b12e129e78
Isabelle <27iornstein@gannacademy.org>
2025-03-03 13:32:19 -0500

    done

--- src/App.java
+++ src/App.java
@@ -5,6 +5,35 @@ public class App extends PApplet{
         PApplet.main("App");
     }
 
+     @Override
+     public void settings() {
+         size(800, 600);
+     }
+ 
+     @Override
+     public void draw() {
+        background(173, 216, 230);
+
+         fill(0, 0, 139);
+         stroke(0);
+         strokeWeight(10);
+         triangle(600, 100, 700, 200, 500, 200);
+
+         ellipse(50, 50, 100, 50);
+
+         strokeWeight(3);
+         line(100, 300, 600, 300);
+
+         noStroke();
+        rect(50, 50, 200, 550);
+
+        fill(255, 0, 0);
+        stroke(255, 0, 0);
+        strokeWeight(5);
+        ellipse(400,300, 50, 50);
+
+     }
+
 
     
 
